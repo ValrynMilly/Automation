79d0f925249ddfc4cdbccd82e2e15da815bfcc10
Emiljano <Emiljanokurbiba@gmail.com>
2021-07-13 02:44:54 +0100

    Update on Pandas Task
    Completed task one.

--- main.py
+++ main.py
@@ -1,37 +1,63 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+from ast import Str
+from typing import List
+from numpy.core.getlimits import _register_known_types
+import pandas as pd #Pandas for data handling
+import matplotlib.pyplot as plt #Matplot for Visuals
 import numpy as np
-print("Pandas: "+pd.__version__)
+from pandas.core.algorithms import isin
+from pandas.core.frame import DataFrame #Numpy for Math
+from pandas.io.pytables import dropna_doc
+print("Pandas: "+pd.__version__) # 1.1.0
 
 #First verify you can read an Excel file
-xls = pd.ExcelFile('Automation\dti2020.xlsx', engine='openpyxl')
-print("Utalizing: "+xls.engine)
+#FP is file path
+fp = pd.ExcelFile('Automation\hiring_muti.xlsx', engine='openpyxl')
+print("Utalizing: "+fp.engine)
 
-#Identifying sheet names
+#Sheet to use identified as 'sh' Prints available sheets in document.
 sh = " "
-print("Sheets: "+sh.join(xls.sheet_names))
+print("Sheets: "+sh.join(fp.sheet_names))
 
-#To use a specific excel sheet in file.
-df = pd.read_excel(xls, sheet_name='Exports')
+#Use a specific excel sheet in file. Making a dataframe for each tab and cleaning data.
+ReportSheet = pd.read_excel(fp, sheet_name='Report2').dropna()
+AccountSheet = pd.read_excel(fp, sheet_name='Sheet1').dropna()
+#print(ReportSheet)
+#print(AccountSheet)
 
-#Plot the information (In this case its a Bar Chart)
-plt.rcdefaults()
-ax = plt.subplot()
+#  #  #  #  #  #  First Task  #  #  #  #  #  #  #  #  
+# Generate new sheet with values from first report minus accounts on the second sheet.
+# To do this I chose to first find the values im looking for & store them.
 
-#Specify specific values to show in dataframe
-values = df[['Country of Destination', 'ValueGBP']].dropna()
+#Skipping first Two rows & cleaning Database Names
+ReportSheet = ReportSheet.iloc[1: , :]
+ReportSheet.columns = ['Trade_Reference','Cpty_Code','Account_Number', 'Counterparty_Fullname','Cpty_Nationality']
 
+#Cleaning Account Sheet Structure
+col1 = AccountSheet.columns[0]
+col2 = AccountSheet.columns[1]
+AccountSheet.columns = ['Account_Number', 'Risk']
+new_row = {'Account_Number':col1, 'Risk':col2} 
+AccountSheet = AccountSheet.append(new_row, ignore_index=True)
 
-# Example data
-y_pos = np.arange(len(values['Country of Destination']))
-Price = 3 + 10 * np.random.rand(len(values['ValueGBP']))
+#Saving all accounts as integers and storing them in a list.
+Accounts = AccountSheet['Account_Number'].astype(int).tolist()
+#Filtering The Data 
+Task_One = ReportSheet[~ReportSheet['Account_Number'].astype(int).isin(Accounts)]
 
+print(ReportSheet)
 
-ax.barh(y_pos, Price, align='center')
-ax.set_yticks(y_pos)
-ax.set_yticklabels(values['Country of Destination'])
-ax.invert_yaxis()  # labels read top-to-bottom
-ax.set_xlabel('Price (£ - GBP)')
-ax.set_title('NON-EU Export Data in the Energy Sector.')
+#The Below code saves changes but only to a new file, even if I specify same file new tab 
+#writer = pd.ExcelWriter('Automation\output.xlsx', engine='openpyxl')
+#Task_One.to_excel(writer, 'Task_One', index = False)
+#writer.save()
 
-plt.show()
+#  #  #  #  #  #  Second Task  #  #  #  #  #  #  #  #  
+blue = [610734, 500364, 610515, 200220]
+red = 600411
+yellow = [100131, 608818, 602484, 605225, 350967]
+
+TaskTwo = ReportSheet.style.apply(lambda x: ['background:blue' if x == blue else 'background:white' for x in ReportSheet['Account_Number'].astype(int)], axis=0)
+
+writer = pd.ExcelWriter('Automation\output.xlsx', engine='openpyxl')
+TaskTwo.to_excel(writer, 'Task_One', index = False)
+writer.save()
