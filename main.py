5b203717aa94bed7d0c4896f0d5a90891847b186
Emiljano <Emiljanokurbiba@gmail.com>
2021-07-19 11:12:35 +0100

    Completed first task
    Second task still not functioning properly, will try multiple methods. One of those methods being to use openpyxl & xlswriter

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from ast import Str
 from typing import List
 from numpy.core.getlimits import _register_known_types
 import pandas as pd #Pandas for data handling
-import matplotlib.pyplot as plt #Matplot for Visuals
 import numpy as np
 from pandas.core.algorithms import isin
 from pandas.core.frame import DataFrame #Numpy for Math
@@ -21,13 +20,12 @@ print("Sheets: "+sh.join(fp.sheet_names))
 #Use a specific excel sheet in file. Making a dataframe for each tab and cleaning data.
 ReportSheet = pd.read_excel(fp, sheet_name='Report2').dropna()
 AccountSheet = pd.read_excel(fp, sheet_name='Sheet1').dropna()
-#print(ReportSheet)
-#print(AccountSheet)
+print(ReportSheet)
+print(AccountSheet)
 
-#  #  #  #  #  #  First Task  #  #  #  #  #  #  #  #  
-# Generate new sheet with values from first report minus accounts on the second sheet.
-# To do this I chose to first find the values im looking for & store them.
-
+  #  #  #  #  #  First Task  #  #  #  #  #  #  #  #  
+#Generate new sheet with values from first report minus accounts on the second sheet.
+#To do this I chose to first find the values im looking for & store them.
 #Skipping first Two rows & cleaning Database Names
 ReportSheet = ReportSheet.iloc[1: , :]
 ReportSheet.columns = ['Trade_Reference','Cpty_Code','Account_Number', 'Counterparty_Fullname','Cpty_Nationality']
@@ -42,22 +40,19 @@ AccountSheet = AccountSheet.append(new_row, ignore_index=True)
 #Saving all accounts as integers and storing them in a list.
 Accounts = AccountSheet['Account_Number'].astype(int).tolist()
 #Filtering The Data 
-Task_One = ReportSheet[~ReportSheet['Account_Number'].astype(int).isin(Accounts)]
+ReportSheet = ReportSheet[~ReportSheet['Account_Number'].astype(int).isin(Accounts)]
 
 print(ReportSheet)
 
 #The Below code saves changes but only to a new file, even if I specify same file new tab 
 #writer = pd.ExcelWriter('Automation\output.xlsx', engine='openpyxl')
-#Task_One.to_excel(writer, 'Task_One', index = False)
+#ReportSheet.to_excel(writer, 'Task_One', index = False)
 #writer.save()
 
 #  #  #  #  #  #  Second Task  #  #  #  #  #  #  #  #  
-blue = [610734, 500364, 610515, 200220]
-red = 600411
-yellow = [100131, 608818, 602484, 605225, 350967]
-
-TaskTwo = ReportSheet.style.apply(lambda x: ['background:blue' if x == blue else 'background:white' for x in ReportSheet['Account_Number'].astype(int)], axis=0)
-
-writer = pd.ExcelWriter('Automation\output.xlsx', engine='openpyxl')
-TaskTwo.to_excel(writer, 'Task_One', index = False)
-writer.save()
+#ReportSheet.style
+#ReportSheet.style.apply(lambda x: ['background:red' if x == 600411 else 'background:white' for x in ReportSheet.Account_Number], axis=0)
+#
+#writer = pd.ExcelWriter("Automation\ddask2.xlsx", engine='openpyxl')
+#ReportSheet.to_excel(writer, 'task2', index = False)
+#writer.save()
